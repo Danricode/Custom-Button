4794d06b8f4b91288730f06ddb9eb9433d4aadf4
Daniel Silva <danielaudrasilva@gmail.com>
2022-05-13 09:41:38 +0200

    Custom Button component with Bootstrap like customization options

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
 
-export default function App() {
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
+const CustomBtn = ({ onPress, title, size, backgroundColor }) => (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        styles.CustomBtnContainer,
+        size === "sm" && {
+          paddingHorizontal: 8,
+          paddingVertical: 6,
+          elevation: 6
+        },
+        backgroundColor && { backgroundColor }
+      ]}
+    >
+      <Text style={[styles.CustomBtnText, size === "sm" && { fontSize: 14 }]}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+  
+  // ...
+  <CustomBtn title="Button" size="sm" backgroundColor="#007bff" />;
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screenContainer}>
+      <CustomBtn title="It works !" size="xl" backgroundColor="red" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 16
   },
+  CustomBtnContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  CustomBtnText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
 });
+
+export default App;
